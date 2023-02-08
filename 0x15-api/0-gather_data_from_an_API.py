785bcb9d34aebd11c983032eb6d679f94ed2a410
bill omole <omolebill01@gmail.com>
2023-02-08 23:06:55 +0100

    added

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-"""this module fetches info from the given url"""
+""" this module fetches info from the given url"""
+
 import requests
 import sys
 
 if __name__ == "__main__":
-    dic = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".
-                       format(sys.argv[1]))
-    dic1 = requests.get("https://jsonplaceholder.typicode.com/users")
+    url = "https://jsonplaceholder.typicode.com/"
+    url_todo = url + "users/{}/todos".format(sys.argv[1])
+    dic = requests.get(url_todo)
+
+    url_users = url + "users/{}".format(sys.argv[1])
+    dic1 = requests.get(url_users)
     todo = dic.json()
-    users = dic1.json()
+    username = dic1.json().get("name")
     lens = len(todo)
-    user = todo[0].get("userId")
 
     tasks = [todo[i].get("completed") for i in range(lens) if
              todo[i].get("completed") is True]
-    username = ""
-    for i in range(len(users)):
-        if users[i].get("id") == user:
-            username = users[i].get("name")
-            break
     print(f"Employee {username} is done with tasks({len(tasks)}/{lens}):")
     for i in range(lens):
         print('\t', ' ', todo[i].get("title"))

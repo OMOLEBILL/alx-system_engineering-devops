6158fa725f31a11ccf07bb8bca5139f173b86b58
bill omole <omolebill01@gmail.com>
2023-02-08 22:33:31 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,22 +3,22 @@
 import requests
 import sys
 
+if __name__ == "__main__":
+    dic = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".
+                       format(sys.argv[1]))
+    dic1 = requests.get("https://jsonplaceholder.typicode.com/users")
+    todo = dic.json()
+    users = dic1.json()
+    lens = len(todo)
+    user = todo[0].get("userId")
 
-dic = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".
-                   format(sys.argv[1]))
-dic1 = requests.get("https://jsonplaceholder.typicode.com/users")
-todo = dic.json()
-users = dic1.json()
-lens = len(todo)
-user = todo[0].get("userId")
-
-tasks = [todo[i].get("completed") for i in range(lens) if
-         todo[i].get("completed") is True]
-username = ""
-for i in range(len(users)):
-    if users[i].get("id") == user:
-        username = users[i].get("name")
-        break
-print(f"Employee {username} is done with tasks({len(tasks)}/{lens}):")
-for i in range(lens):
-    print('\t', ' ', todo[i].get("title"))
+    tasks = [todo[i].get("completed") for i in range(lens) if
+             todo[i].get("completed") is True]
+    username = ""
+    for i in range(len(users)):
+        if users[i].get("id") == user:
+            username = users[i].get("name")
+            break
+    print(f"Employee {username} is done with tasks({len(tasks)}/{lens}):")
+    for i in range(lens):
+        print('\t', ' ', todo[i].get("title"))

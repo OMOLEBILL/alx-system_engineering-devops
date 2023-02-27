32d640af5a3d9285d7130b46865de89eb143a6f1
bill omole <omolebill01@gmail.com>
2023-02-27 23:02:20 +0100

    almost

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,9 @@ import requests
 def number_of_subscribers(subreddit):
     """args a subrediit name
     return the no of subscribers"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent':
-               "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36\
-                       (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    header = {"User-Agent": "My-User-Agent"}
+    response = requests.get(url, headers=header, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         number = data.get("data").get("subscribers")
